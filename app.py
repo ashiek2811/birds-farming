a5fe5ff3d665028fdaf4919c134747bcdfb3d63c
ashiek2811 <you@example.com>
2023-12-01 10:12:00 +0530

    shadow

--- app.py
+++ app.py
@@ -3,9 +3,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-   return render_template('home.html',name="ashiek")
+   return render_template('login.html')
+
+@app.route('/home')
+def home():
+   return render_template('home.html')
 
 
+
+@app.route('/about')
+def about():
+   return render_template('about.html')
 
 
 if __name__ == '__main__':
